6f60ac0117c2580cb7b68fa20e1c3bd319bf4eac
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-15 11:06:44 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public interface IName
+{
+	string GetName();
+}
+
+public interface INumber
+{
+	string GetNumber();
+}
+
+public class Person : IName, INumber
+{
+	public string GetName()
+	{
+		return "Person";
+	}
+
+	public string GetNumber()
+	{
+		return "09**4112";
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Class_0515_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IName p1 = new Person();
+            Console.WriteLine(p1.GetName());
+            INumber p2= new Person();
+            Console.WriteLine(p2.GetNumber());
+        }
+    }
+}
